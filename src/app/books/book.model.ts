51bc84ba8b3dcc4335119613a10ccab262bc4586
Katrin Valge <katrin.valge@mail.ee>
2023-01-02 18:57:32 +0200

    Created book model.

---
+++ src/app/books/book.model.ts
@@ -0,0 +1,19 @@
+export class BookModel {
+
+  public id: number;
+  public name: string;
+  public acquisitionDate: Date;
+  public copyQuantity: number;
+  public loanPeriod: number;
+  public location: string;
+
+
+  constructor(id: number, name: string, acquisitionDate: Date, copyQuantity: number, loanPeriod: number, location: string) {
+    this.id = id;
+    this.name = name;
+    this.acquisitionDate = acquisitionDate;
+    this.copyQuantity = copyQuantity;
+    this.loanPeriod = loanPeriod;
+    this.location = location;
+  }
+}
