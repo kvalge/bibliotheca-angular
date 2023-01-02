706f2c0ae7d407b7dc4511825e487345ddafb3fb
Katrin Valge <katrin.valge@mail.ee>
2023-01-02 18:26:56 +0200

    Created footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BooksComponent } from './books/books/books.component';
 import { BookListComponent } from './books/books/book-list/book-list.component';
 import { HeaderComponent } from './layout/header/header.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
     BookListComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
